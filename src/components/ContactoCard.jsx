3d4153ade0819693def3ea4bda149ee90a6d6a82
estebanvalcarcel6909 <estebanvalcarcel368@gmail.com>
2026-08-19 17:14:11 -0500

    clase 6 tailwind

--- src/components/ContactoCard.jsx
+++ src/components/ContactoCard.jsx
@@ -1,15 +1,38 @@
-export default function ContactoCard({ nombre, telefono, correo, etiqueta, onEliminar }) {
-  return (
-    <article className="tarjeta-contacto">
-      <h3>{nombre}</h3>
-      <p>📞 {telefono}</p>
-      <p>✉️ {correo}</p>
-      {etiqueta && <p>{etiqueta}</p>}
-      <div className="acciones">
-        <button className="btn-eliminar" onClick={() => onEliminar(correo)}>
-          Eliminar
-        </button>
-      </div>
-    </article>
-  );
+// src/components/ContactoCard.jsx
+export default function ContactoCard({
+ nombre,
+ telefono,
+ correo,
+ etiqueta,
+ onEliminar
+}) {
+ return (
+ <article className="bg-white border border-gray-200 rounded-xl shadow-sm p-5">
+ {/* Nombre destacado */}
+ <h3 className="text-xl font-bold text-gray-900 mb-3">
+ {nombre}
+ </h3>
+ {/* Teléfono */}
+ <h3 className="text-x font-bold text-gray-900 mb-2">
+ {telefono}
+ </h3>
+ <h3 className="text-x font-bold text-gray-900 mb-2">
+ {correo}
+ </h3>
+ <h3 className="text-x font-bold text-gray-900 mb-2">
+ {etiqueta}
+ </h3>
+ <p className="text-gray-700 mb-1"></p>
+  {/* Acciones (alineado a la izquierda en móvil) */}
+ <div className="flex justify-start">
+ <button
+ onClick={() => onEliminar(correo)}
+ className="bg-red-500 hover:bg-red-600 text-white text-sm font-medium
+px-4 py-2 rounded-lg transition-colors"
+ >
+ Eliminar
+ </button>
+ </div>
+ </article>
+ );
 }
